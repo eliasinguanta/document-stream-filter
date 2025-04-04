dfb8d0ec98fa24d79df58ba7ce744254c8ebb8a2
eliasinguanta <inguanta@campus.tu-berlin.de>
2025-04-04 14:21:58 +0000

    debug container starting script

--- start_server.sh
+++ start_server.sh
@@ -4,7 +4,7 @@
 PUBLIC_IP=$(curl -s http://checkip.amazonaws.com || echo "localhost")
 
 # make ip available for vite
-echo "VITE_API_URL=https://$PUBLIC_IP:3000" > /client/.env
+echo "VITE_API_URL=https://$PUBLIC_IP:3000" > ./client/.env
 
 # start the server
 npm run start
